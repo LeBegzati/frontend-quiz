c49a5d3ec37060345ec9f2b6fc41a41f6829a833
Leonit <begzati.leonit@johak.at>
2024-11-15 09:59:34 +0100

    Begzati

---
+++ src/routes/[quiz]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+	let { data } = $props();
+	let currentQuestion = $state(0);
+</script>
+
+{#each data.questions as question, index (question.question)}
+	{#if index === currentQuestion}
+		<p>Question {index + 1} of {data.questions.length}</p>
+		<h2>{question.question}</h2>
+	{/if}
+{/each}
+
+<button class="btn btn-primary" onclick={() => currentQuestion++}>Submit</button>
