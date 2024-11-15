13fdbd0edbb629eca90af01e14ab857049a65dbe
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-11-15 10:36:15 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	let { data } = $props();
 </script>
 
-<h1>Welcome to the <strong class="text-primary">Frontend Quiz!</strong></h1>
+<h1>Welcome to the <strong class="text-primary">Prof. Seiwald's Frontend Quiz!</strong></h1>
 <p>Pick a subject to get started.</p>
 
 <div class="flex flex-col gap-4">
