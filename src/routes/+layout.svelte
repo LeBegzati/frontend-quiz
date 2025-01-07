03061df5fb9f62dda4cd32c402f6967c26d20096
Leonit <begzati.leonit@johak.at>
2025-01-07 12:17:16 +0100

    fertig

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,31 @@
 <script>
 	import '../app.css';
 	import 'iconify-icon';
+	import { fade } from 'svelte/transition';
+	/**
+	 * @typedef {Object} Props
+	 * @property {import('svelte').Snippet} [children]
+	 */
 
-	let theme = 'dark';
+	/** @type {Props} */
+	let { children, data } = $props();
+
+	let theme = $state('dark');
 
 	function switchTheme() {
-		theme = theme === 'dark' ? 'light' : 'dark';
+		if (theme === 'dark') {
+			theme = 'light';
+			localStorage.setItem('theme', 'light');
+		} else {
+			theme = 'dark';
+			localStorage.setItem('theme', 'dark');
+		}
 	}
+
+	$effect(() => {
+		if (localStorage.getItem('theme')) theme = localStorage.getItem('theme');
+		if (theme === 'light') document.querySelector('.toggle').checked = false;
+	});
 </script>
 
 <div data-theme={theme} class="min-h-screen p-4">
@@ -17,10 +36,14 @@
 				type="checkbox"
 				class="toggle border-none bg-white [--tglbg:#a729f5] hover:bg-white"
 				checked="checked"
-				on:click={switchTheme}
+				onclick={switchTheme}
 			/>
 			<iconify-icon icon="lucide-moon" class="text-2xl"></iconify-icon>
 		</div>
 	</div>
-	<slot />
+	{#key data.url}
+		<div in:fade={{ delay: 200, duration: 200 }} out:fade={{ duration: 200 }}>
+			{@render children?.()}
+		</div>
+	{/key}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,13 @@
 <script>
 	import ListItem from '$lib/fonts/components/ListItem.svelte';
-	export let data;
+	let { data } = $props();
 </script>
 
-<h1>Welcome to the <strong class="text-primary">Frontend Quiz!</strong></h1>
-<p>Pick a subject to get started</p>
+<h1>Welcome to the <strong class="text-primary">Leonit's Frontend Quiz!</strong></h1>
+<p>Pick a subject to get started.</p>
 
 <div class="flex flex-col gap-4">
 	{#each data.quizzes as quiz, index (quiz.title)}
-		<a href="/{quiz.title.toLowerCase()}"><ListItem title={quiz.title} {index} /></a>
+		<a href="/{quiz.title.toLowerCase()}"><ListItem title={quiz.title} icon={quiz.icon} /></a>
 	{/each}
 </div>
-
-<!-- Open the modal using ID.showModal() method  <button class="btn" onclick="my_modal_1.showModal()">...and DaisyUI (click)</button>
-<dialog id="my_modal_1" class="modal">
-	<div class="modal-box">
-		<h3 class="text-lg font-bold">Hello from Daisy 🌻!</h3>
-		<p class="py-4">Press ESC key or click the button below to close</p>
-		<div class="modal-action">
-			<form method="dialog">
-				 if there is a button in form, it will close the modal 
-				<button class="btn">Close</button>
-			</form>
-		</div>
-	</div>
-</dialog> -->
